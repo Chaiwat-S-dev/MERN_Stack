42bb9ded75eaa198ccf1355fb883d7ebce344d36
chaiwat <chaiwat.s@technimal.co.th>
2021-03-30 07:36:07 +0700

    init project MERN stack

---
+++ src/components/create-student.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreatStudent extends Component {
+    render() {
+        return (
+            <div>
+                <p>React creat student component!</p>
+            </div>
+        )
+    }
+}
